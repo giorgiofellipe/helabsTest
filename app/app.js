7fff7c2df0f52bb3dd02978bacb49151f37be10f
Giorgio Fellipe <giorgiofellipe@gmail.com>
2015-06-11 21:21:15 -0300

    Add previous phases

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ angular.module('myApp', [
   'pascalprecht.translate',
   'toggle-switch'
 ])
-.config(['$routeProvider', '$translateProvider', function($routeProvider, $translateProvider) {
+.config(['$routeProvider', '$translateProvider', '$httpProvider', function($routeProvider, $translateProvider, $httpProvider) {
     $routeProvider.when('/home', {
       templateUrl: '/components/home/home.html',
       controller: 'HomeCtrl'
@@ -18,14 +18,41 @@ angular.module('myApp', [
       prefix: '/languages/',
       suffix: '.json'
     });
+
+    $httpProvider.defaults.transformResponse.push(function(responseData){
+      convertDateStringsToDates(responseData);
+      return responseData;
+    });
+
+    var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+
+    function convertDateStringsToDates(input) {
+      // Ignore things that aren't objects.
+      if (typeof input !== "object") return input;
+
+      for (var key in input) {
+        if (!input.hasOwnProperty(key)) continue;
+
+        var value = input[key];
+        var match;
+        // Check for string properties which look like dates.
+        if (typeof value === "string" && (match = value.match(regexIso8601))) {
+          var milliseconds = Date.parse(match[0])
+          if (!isNaN(milliseconds)) {
+            input[key] = new Date(milliseconds);
+          }
+        } else if (typeof value === "object") {
+          // Recurse into object
+          convertDateStringsToDates(value);
+        }
+      }
+    }
 }])
 .run(['$localstorage', '$translate', function($localstorage, $translate) {
     var language = $localstorage.get('language');
-    console.log(language);
-    //if (!language) {
-    //  $localstorage.set('language', 'en_US');
-    //  language = $localstorage.get('language');
-    //}
-    console.log(language);
+    if (!language) {
+      $localstorage.set('language', 'en_US');
+      language = $localstorage.get('language');
+    }
     $translate.preferredLanguage(language);
 }]);
